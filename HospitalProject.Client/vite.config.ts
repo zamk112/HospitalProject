ffbfc2fd2e22042d9b53535ae17884d76a21048b
Zamsheed Khan <zamk112@gmail.com>
2026-04-09 00:19:42 +1000

    02_SettingUpForLocalDevelopment Initial Commit * Added 02_SettingUpForLocalDevelopment.md * Updated README.md and reference to 02_SettingUpForLocalDevelopment.md * Updated vite.config.ts and added SSL and reverse proxy configuration. * Updated App.tsx, added fetch call to weatherforecast endpoint and rendered data as a table. * Updated appsettings.Development.json & appsettings.json with Serialog configuration. * Add Serialog package and code for logging in Program.cs. * Removed HospitalProject.Server.http because it's not needed. * Updated launchSettings.json and removed http url from https profile. * Added launch.json and task.json VS code configurations.

--- HospitalProject.Client/vite.config.ts
+++ HospitalProject.Client/vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { readFileSync, existsSync } from 'node:fs';
+import os from "node:os";
+import { env } from "node:process";
+import https from "https";
 
-// https://vite.dev/config/
+const certName = 'hospitalproject.SSC';
+const certFolder = path.join(os.homedir(), 'Workspaces', 'Certs', 'dotnet');
+const certPath = path.join(certFolder, `${certName}.pem`);
+const keyPath = path.join(certFolder, `${certName}.key`);
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` : 'https://localhost:7083';
+
+
+if (!existsSync(certPath) || !existsSync(keyPath)) {
+  throw new Error('Certificate not found.');
+}
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: readFileSync(keyPath),
+      cert: readFileSync(certPath)
+    },
+    proxy: {
+      '^/weatherforecast': {
+        target: target,
+        secure: true,
+        agent: new https.Agent({
+          ca: readFileSync(certPath)
+        })
+      }
+    }
+  },
 })
--- HospitalProject.Server/Program.cs
+++ HospitalProject.Server/Program.cs
@@ -1,23 +1,65 @@
+using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    builder.Services.AddSerilog((services, lc) => lc
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+    );
+    
+    if (builder.Environment.IsDevelopment())
+    {
+        builder.Services.AddHttpLogging(o => { 
+            o.LoggingFields = HttpLoggingFields.All; 
+            o.RequestHeaders.Add("Referer");
+            o.RequestHeaders.Add("sec-ch-ua-platform");
+            o.RequestHeaders.Add("sec-ch-ua");
+            o.RequestHeaders.Add("sec-ch-ua-mobile");
+            o.RequestHeaders.Add("sec-fetch-site");
+            o.RequestHeaders.Add("sec-fetch-mode");
+            o.RequestHeaders.Add("sec-fetch-dest");
+            o.RequestHeaders.Add("priority");
+        });
+    }
+
+    builder.Services.AddControllers();
+    
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+    Log.Information("Application started! Logging to both console and/or file.");
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseHttpLogging();
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start-up failed!");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
